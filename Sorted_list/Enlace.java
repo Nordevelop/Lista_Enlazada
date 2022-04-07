dd7d50f240673fca639304b4a155afe47c2ac615
Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
2022-04-07 19:10:45 -0300

    Update Enlace.java

--- Sorted_list/Enlace.java
+++ Sorted_list/Enlace.java
@@ -4,11 +4,11 @@ public class Enlace {
 	
 		public long Data1; // data item
 		public Enlace siguiente; // next link in list
-		// -------------------------------------------------------------
+
 		public Enlace(long d1) // constructor
 		{ Data1 = d1; }
-		// -------------------------------------------------------------
-		public void mostrarenlace() // display this link
+		
+		public void mostrarenlace() 
 		{ System.out.print(Data1 + " "); }
 	
 
