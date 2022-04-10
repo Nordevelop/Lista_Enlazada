e81ab1512ef7534fb9e411d62abba233df393a05
Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
2022-04-10 12:43:01 -0300

    Update SortedLista.java

--- Sorted_list/SortedLista.java
+++ Sorted_list/SortedLista.java
@@ -2,7 +2,7 @@ package misClases;
 import java.util.*;
 
 public class SortedLista {
-	private Enlace primero; // ref tal primer item  en la lista
+	private Enlace primero; // ref al primer item  en la lista
 
 	public SortedLista() // constructor
 	{ primero = null; }
@@ -16,39 +16,35 @@ public class SortedLista {
 	Enlace newLink = new Enlace(key); // 
 	Enlace previous = null; 
 	Enlace current = primero; 
-	while(current != null && key > current.Data1){ // o key > current,
+	while(current != null && key > current.Data1){ // campo clave > nodo_actual key > current,
 	previous = current;
 	current = current.siguiente; // ir al siguiente elemento
 	}
-	if(previous==null) // al comenzar de la lista
-		primero = newLink; // primero --> newLink
+	if(previous==null) 
+		primero = newLink;
 	else // not at beginning
-	previous.siguiente = newLink; // old prev --> newLink
-	newLink.siguiente = current; // newLink --> old current
+	previous.siguiente = newLink; 
+	newLink.siguiente = current; 
 	} // end insert()
-	// -------------------------------------------------------------
-	public Enlace borrar() // return & delete primero link
-	{ // (assumes non-empty list)
+	
+	public Enlace borrar(){ //retorna variable temporal y borra enlace del primer punntero
+	 // (assumes non-empty list)
 	Enlace temp = primero; // save primero
 	primero = primero.siguiente; // delete primero
 	return temp; // return value
 	}
-	// -------------------------------------------------------------
+	
 	public void mostrarLista(){
-		
 		System.out.print("Lista Ordenada (primero-->ultimo): ");
 		Enlace current = primero; // start at beginning of list
-		while(current != null){ // until end of list,
-			current.mostrarenlace(); // print data1
-			current = current.siguiente; // move to next link
+		while(current != null){ 
+			current.mostrarenlace(); 
+			current = current.siguiente; 
 	}
 	System.out.println(" ");
 	}
-	
-		
-	
 	} // end class SortedList
-	////////////////////////////////////////////////////////////////
+	
 	
 	
 
